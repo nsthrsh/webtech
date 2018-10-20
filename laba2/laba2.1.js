48ef6c091ff11f13169f81d1e90b548e5b87030c
nsthrsh <44110480+nsthrsh@users.noreply.github.com>
2018-10-20 13:31:35 +0300

    Add files via upload

---
+++ laba2/laba2.1.js
@@ -0,0 +1,15 @@
+﻿var name ="Anastasiia Hrysh";
+alert(name);
+var vari ="Variant 4";
+alert(vari);
+
+//task1
+function cube(numb){
+	return(numb*numb*numb)
+}
+
+alert("Task 1")
+var numb;
+numb=prompt("Enter number of your variant: ");
+
+alert("Your variant in cube: " +cube(numb));
